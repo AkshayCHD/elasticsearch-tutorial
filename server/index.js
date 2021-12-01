1d4d2f9239e0382d7841073a63f0f8df560e7e6d
Akshay Kumar <akshay.kumar@smallcase.com>
2021-12-01 22:49:37 +0530

    Remove mongodb logic

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
-const mongoose = require("mongoose");
 const express = require("express");
 const cors = require("cors");
-const Review = require("./model/mReviewModel");
 var elasticsearch = require("elasticsearch");
 
 const app = express();
@@ -9,23 +7,6 @@ var client = new elasticsearch.Client({
   host: "localhost:9200",
   log: "trace",
   apiVersion: "7.2", // use the same version of your Elasticsearch instance
-});
-const username = "root";
-const password = "example";
-const dbname = "mongo-elastic";
-
-mongoose.connect(
-  `mongodb://${username}:${password}@localhost:27017/${dbname}?authSource=admin&readPreference=primary`,
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
-
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", async function () {
-  console.log("Connected successfully");
 });
 
 const parseElasticResponse = (elasticResponse) => {
@@ -60,30 +41,6 @@ app.get("/elastic", async (req, res, next) => {
     res.json({
       message: "Searched Successfully",
       records: parseElasticResponse(response),
-    });
-  } catch (err) {
-    next(err);
-  }
-});
-
-app.get("/mongo", async (req, res, next) => {
-  try {
-    const { text } = req.query;
-    console.log(text);
-    if (!text) {
-      res.json({
-        message: "Searched Successfully",
-        records: [],
-      });
-    }
-    const regex = new RegExp(text);
-    const response = await Review.find({
-      $or: [{ name: regex }, { space: regex }, { description: regex }, { summary: regex }, { notes: regex }],
-    }).limit(50).lean();
-    console.log(response)
-    res.json({
-      message: "Searched Successfully",
-      records: response,
     });
   } catch (err) {
     next(err);
